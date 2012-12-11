openembedded/meta-openembedded

commit 3becce8504033dfac7c497fa12675f873a5df9e0

openbox: separate package for configuration files (openbox-config)
Change-Id: I528392100854fb73c2a14973fd1bfa9c94e836a7
Signed-off-by: Mario Domenech Goulart <mario@ossystems.com.br>
Signed-off-by: Koen Kooi <koen@dominion.thruhere.net>

meta-oe/recipes-graphics/openbox/openbox_3.5.0.bb
@@ -9,6 +9,8 @@ SRC_URI = "http://icculus.org/openbox/releases/openbox-${PV}.tar.gz"
 SRC_URI[md5sum] = "00441b53cf14c03566c8e82643544ff9"
 SRC_URI[sha256sum] = "6fa90016530b3aa6102e254079461977439398531fb23e7ec076ff2c140fea0a"
 
+PR = "1"
+
 inherit autotools gettext update-alternatives
 
 ALTERNATIVE_PATH = "${bindir}/openbox"
@@ -18,7 +20,7 @@ ALTERNATIVE_PRIORITY = "10"
 
 EXTRA_OECONF += "--with-plugins=none"
 
-PACKAGES =+ "${PN}-core ${PN}-lxde ${PN}-gnome"
+PACKAGES =+ "${PN}-core ${PN}-lxde ${PN}-gnome ${PN}-config"
 
 PACKAGES_DYNAMIC += "^${PN}-theme-.*"
 
@@ -28,7 +30,7 @@ python populate_packages_prepend() {
     do_split_packages(d, theme_dir, '(.*)', theme_name, '${PN} theme for %s', extra_depends='', allow_dirs=True)
 }
 
-RDEPENDS_${PN} += "${PN}-core"
+RDEPENDS_${PN} += "${PN}-core ${PN}-config"
 FILES_${PN}-core = "${bindir}/openbox ${libdir}/*${SOLIBS}"
 
 FILES_${PN}-lxde += "${datadir}/lxde/ \
@@ -37,3 +39,5 @@ FILES_${PN}-lxde += "${datadir}/lxde/ \
                      ${datadir}/icons"
 
 FILES_${PN}-gnome += "${datadir}/gnome/"
+
+FILES_${PN}-config += "${sysconfdir}"

commit 49c232118052aeef2336aafff464603cf6771eb9

openbox: fix PR format and use new update-alternatives syntax
* fixes:
  NOTE: recipe openbox-3.5.0-1: task do_package: Started
  WARNING: openbox: Use of ALTERNATIVE_NAME is deprecated, see
  update-alternatives.bbclass for more info.
* bump PE if you care about upgrade path from wrong PR, introduced in
  commit 3becce8504033dfac7c497fa12675f873a5df9e0
  Author: Mario Domenech Goulart <mario@ossystems.com.br>
  Date:   Mon Dec 3 16:03:23 2012 +0000

Signed-off-by: Martin Jansa <Martin.Jansa@gmail.com>

meta-oe/recipes-graphics/openbox/openbox_3.5.0.bb
@@ -9,14 +9,13 @@ SRC_URI = "http://icculus.org/openbox/releases/openbox-${PV}.tar.gz"
 SRC_URI[md5sum] = "00441b53cf14c03566c8e82643544ff9"
 SRC_URI[sha256sum] = "6fa90016530b3aa6102e254079461977439398531fb23e7ec076ff2c140fea0a"
 
-PR = "1"
+PR = "r2"
 
 inherit autotools gettext update-alternatives
 
-ALTERNATIVE_PATH = "${bindir}/openbox"
-ALTERNATIVE_NAME = "x-window-manager"
-ALTERNATIVE_LINK = "${bindir}/x-window-manager"
-ALTERNATIVE_PRIORITY = "10"
+ALTERNATIVE_${PN}-core = "x-window-manager"
+ALTERNATIVE_TARGET[x-window-manager] = "${bindir}/openbox"
+ALTERNATIVE_PRIORITY[x-window-manager] = "10"
 
 EXTRA_OECONF += "--with-plugins=none"
 