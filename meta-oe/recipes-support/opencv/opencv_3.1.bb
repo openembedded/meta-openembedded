openembedded/meta-openembedded

commit 3132af1d1d15209f3512eba8d8885b909b3b13ca

opencv: Fix metapkg dependencies for opencv-java and opencv-locales
The opencv-java is not always generated so we ought to include it,
only if it is available. The opencv-locales is not available and ought
to be skipped.

Signed-off-by: Otavio Salvador <otavio@ossystems.com.br>
Signed-off-by: Martin Jansa <Martin.Jansa@gmail.com>

meta-oe/recipes-support/opencv/opencv_3.1.bb
@@ -79,7 +79,8 @@ export ANT_DIR="${STAGING_DIR_NATIVE}/usr/share/ant/"
 
 TARGET_CC_ARCH += "-I${S}/include "
 
-PACKAGES += "${PN}-java-dbg ${PN}-java ${PN}-samples-dbg ${PN}-samples ${PN}-apps python-opencv"
+PACKAGES += "${PN}-samples-dbg ${PN}-samples ${PN}-apps python-opencv \
+             ${@bb.utils.contains('PACKAGECONFIG', 'oracle-java', '${PN}-java-dbg ${PN}-java', '', d)}"
 
 python populate_packages_prepend () {
     cv_libdir = d.expand('${libdir}')
@@ -104,7 +105,7 @@ python populate_packages_prepend () {
     blacklist = [ metapkg ]
     metapkg_rdepends = [ ]
     for pkg in packages[1:]:
-        if not pkg in blacklist and not pkg in metapkg_rdepends and not pkg.endswith('-dev') and not pkg.endswith('-dbg') and not pkg.endswith('-doc') :
+        if not pkg in blacklist and not pkg in metapkg_rdepends and not pkg.endswith('-dev') and not pkg.endswith('-dbg') and not pkg.endswith('-doc') and not pkg.endswith('-locale'):
             metapkg_rdepends.append(pkg)
     bb.data.setVar('RDEPENDS_' + metapkg, ' '.join(metapkg_rdepends), d)
 

commit b27938bc7ac6ab9eb2c31db15ec787c7b4a5a55d

opencv: Reorder PACKAGES variable
PACKAGES content order is important, because it determines the
destination of the files, when the folders overlaps.

Without this patch:

NOTE: Executing RunQueue Tasks
ERROR: QA Issue: non debug package contains .debug directory:
opencv-apps path
packages-split/opencv-apps/usr/share/OpenCV/java/.debug/libopencv_java310.so
[debug-files]
WARNING: QA Issue: opencv-apps: found library in wrong location:
/usr/share/OpenCV/java/libopencv_java310.so
opencv-apps: found library in wrong location:
/usr/share/OpenCV/java/.debug/libopencv_java310.so [libdir]
ERROR: QA run found fatal errors. Please consider fixing them.
ERROR: Function failed: do_package_qa

Fixes: 3132af1d1d15 ("opencv: Fix metapkg dependencies for opencv-java ... ")

Acked-by: Otavio Salvador <otavio@ossystems.com.br>
Signed-off-by: Ricardo Ribalda Delgado <ricardo.ribalda@gmail.com>
Signed-off-by: Martin Jansa <Martin.Jansa@gmail.com>

meta-oe/recipes-support/opencv/opencv_3.1.bb
@@ -78,8 +78,8 @@ export ANT_DIR="${STAGING_DIR_NATIVE}/usr/share/ant/"
 
 TARGET_CC_ARCH += "-I${S}/include "
 
-PACKAGES += "${PN}-samples-dbg ${PN}-samples ${PN}-apps python-opencv \
-             ${@bb.utils.contains('PACKAGECONFIG', 'oracle-java', '${PN}-java-dbg ${PN}-java', '', d)}"
+PACKAGES += "${@bb.utils.contains('PACKAGECONFIG', 'oracle-java', '${PN}-java-dbg ${PN}-java', '', d)} \
+             ${PN}-samples-dbg ${PN}-samples ${PN}-apps python-opencv"
 
 python populate_packages_prepend () {
     cv_libdir = d.expand('${libdir}')