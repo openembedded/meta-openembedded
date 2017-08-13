openembedded/meta-openembedded

commit 187798c3bd3dad2de3d159faf456aa6e71fbdf46

gvfs: update 1.28.3 -> 1.32.1
Signed-off-by: Andreas Müller <schnitzeltony@googlemail.com>
Signed-off-by: Martin Jansa <Martin.Jansa@gmail.com>

meta-gnome/recipes-gnome/gvfs/gvfs_1.32.1.bb
@@ -2,14 +2,14 @@ DESCRIPTION = "gvfs is a userspace virtual filesystem"
 LICENSE = "LGPLv2"
 LIC_FILES_CHKSUM = "file://COPYING;md5=05df38dd77c35ec8431f212410a3329e"
 
-inherit gnome bash-completion
+inherit gnome bash-completion gettext
 
 DEPENDS += "libsecret glib-2.0 gconf intltool-native libgudev udisks2"
 
 SRC_URI = "https://download.gnome.org/sources/${BPN}/${@gnome_verdir("${PV}")}/${BPN}-${PV}.tar.xz;name=archive"
 
-SRC_URI[archive.md5sum] = "d2399a1a217dd5030e8830c7e7c813c2"
-SRC_URI[archive.sha256sum] = "458c4cb68570f6ef4a9e152995c62d0057c3e0a07ed64d84c7200cdd22f0bd17"
+SRC_URI[archive.md5sum] = "cbe766b46f324e17d7abcfb4a89a1684"
+SRC_URI[archive.sha256sum] = "d0b6c9edab09d52472355657a2f0a14831b2e6c58caba395f721ab683f836ade"
 
 EXTRA_OECONF = " \
     --disable-gdu \

commit 664e550c767547fb0767c48a48e6b74a54cf4c78

gvfs: Fix build regression from uprev
The commit 187798c3bd3dad2de3d159faf456aa6e71fbdf46 (gvfs: update
1.28.3 -> 1.32.1) caused a regression where images that use rpm4 can
no longer assemble correctly because it includes polkit rules.

At image creation time rpm4 will detect a mismatch of the ownership
with the polkit rules.d directory as follows:

    file /usr/share/polkit-1/rules.d conflicts between attempted installs of gvfs-1.32.1-r0.1.core2_64 and polkit-0.113-r0.8.core2_64
    file /usr/share/polkit-1/rules.d conflicts between attempted installs of libvirt-1.3.5-r0.9.core2_64 and gvfs-1.32.1-r0.1.core2_64

The libvirt which also appears in the error message has already been
fixed in the meta-virtualization layer in a similar maner. (http://git.yoctoproject.org/cgit/cgit.cgi/meta-virtualization/commit/?id=67c86c4962170354ad8a2ec4cdbb9bc0056cfd66)

Signed-off-by: Jason Wessel <jason.wessel@windriver.com>
Signed-off-by: Martin Jansa <Martin.Jansa@gmail.com>

meta-gnome/recipes-gnome/gvfs/gvfs_1.32.1.bb
@@ -4,7 +4,7 @@ LIC_FILES_CHKSUM = "file://COPYING;md5=05df38dd77c35ec8431f212410a3329e"
 
 inherit gnome bash-completion gettext
 
-DEPENDS += "libsecret glib-2.0 gconf intltool-native libgudev udisks2"
+DEPENDS += "libsecret glib-2.0 gconf intltool-native libgudev udisks2 polkit shadow-native"
 
 SRC_URI = "https://download.gnome.org/sources/${BPN}/${@gnome_verdir("${PV}")}/${BPN}-${PV}.tar.xz;name=archive"
 
@@ -58,3 +58,9 @@ PACKAGECONFIG[fuse] = "--enable-fuse, --disable-fuse, fuse"
 
 # libcdio-paranoia recipe doesn't exist yet
 PACKAGECONFIG[cdda] = "--enable-cdda, --disable-cdda, libcdio-paranoia"
+
+# Fix up permissions on polkit rules.d to work with rpm4 constraints
+do_install_append() {
+	chmod 700 ${D}/${datadir}/polkit-1/rules.d
+	chown polkitd:root ${D}/${datadir}/polkit-1/rules.d
+}