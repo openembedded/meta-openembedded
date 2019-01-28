openembedded/meta-openembedded

commit 97a1a55f475510569590f9b8ad2786bdb34f0319

polkit: add polkit as a required distro feature
Make sure polkit is in DISTRO_FEATURERS if this package is being
installed. This will make sure that people who do use polkit in
their image also do enabled the recently introduced distro feature
polkit in their distro.

Signed-off-by: Stefan Agner <stefan.agner@toradex.com>
Signed-off-by: Khem Raj <raj.khem@gmail.com>

meta-oe/recipes-extended/polkit/polkit_0.115.bb
@@ -9,6 +9,8 @@ DEPENDS = "expat glib-2.0 intltool-native mozjs"
 
 inherit autotools gtk-doc pkgconfig useradd systemd gobject-introspection
 
+REQUIRED_DISTRO_FEATURES = "polkit"
+
 PACKAGECONFIG = "${@bb.utils.filter('DISTRO_FEATURES', 'pam', d)} \
                  ${@bb.utils.contains('DISTRO_FEATURES', 'systemd', 'systemd', \
                     bb.utils.contains('DISTRO_FEATURES', 'x11', 'consolekit', '', d), d)} \

commit c049e02663d2843b38eb92e6c8e4efa5014594fc

polkit: inherit distro_features_check
Make the recently added REQUIRED_DISTRO_FEATURES effective by
inheriting distro_features_check.

Fixes: 97a1a55f4755 ("polkit: add polkit as a required distro feature")
Signed-off-by: Stefan Agner <stefan.agner@toradex.com>
Signed-off-by: Khem Raj <raj.khem@gmail.com>

meta-oe/recipes-extended/polkit/polkit_0.115.bb
@@ -7,7 +7,7 @@ LIC_FILES_CHKSUM = "file://COPYING;md5=155db86cdbafa7532b41f390409283eb \
 
 DEPENDS = "expat glib-2.0 intltool-native mozjs"
 
-inherit autotools gtk-doc pkgconfig useradd systemd gobject-introspection
+inherit autotools gtk-doc pkgconfig useradd systemd gobject-introspection distro_features_check
 
 REQUIRED_DISTRO_FEATURES = "polkit"
 