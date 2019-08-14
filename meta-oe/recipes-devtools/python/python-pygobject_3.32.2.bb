openembedded/meta-openembedded

commit f8e1d0c17658d1b41d201c63b285d10d9aa8cfc6

python-pygobject: move python-setuptools from RDEPENDS to DEPENDS
python-setuptools is necessary to build pygobject but not to run it, as
also documented by the upstream packaging guide:

https://gitlab.gnome.org/GNOME/pygobject/blob/master/docs/packagingguide.rst

Move it to DEPENDS to shrink the runtime dependency chain, as setuptools
includes scripts to download other packages, which might not be desired on
a minimal image.

Signed-off-by: Luca Boccassi <luca.boccassi@microsoft.com>
Signed-off-by: Khem Raj <raj.khem@gmail.com>

meta-oe/recipes-devtools/python/python-pygobject_3.32.2.bb
@@ -7,7 +7,7 @@ LIC_FILES_CHKSUM = "file://COPYING;md5=a916467b91076e631dd8edb7424769c7"
 GNOMEBASEBUILDCLASS = "meson"
 inherit gnomebase gobject-introspection distutils-base upstream-version-is-even
 
-DEPENDS += "python glib-2.0"
+DEPENDS += "python python-setuptools glib-2.0"
 
 SRCNAME = "pygobject"
 SRC_URI = " \
@@ -30,7 +30,6 @@ PACKAGECONFIG[python] = "-Dpython=python3, -Dpython=python2, , "
 PACKAGECONFIG[stagedir] = "-Dstagedir=${PYTHON_SITEPACKAGES_DIR}, -Dstagedir="", , "
 
 BBCLASSEXTEND = "native"
-RDEPENDS_${PN} = "python-setuptools"
 RDEPENDS_${PN}_class-native = ""
 
 do_install_append() {

commit d21d509b9f643371e9717a9a2c73607126d6029e

python-pygobject: remove build-dependency on setuptools and add dependency on pkgutil
Sync python-pygobject recipe with poky's python3-pygobject:
Setuptools is not a mandatory build-dependency, so remove it.
Pkgutil from the core library is needed at runtime, so add a
dependency on it.

Fixes: f8e1d0c17658 ("python-pygobject: move python-setuptools from RDEPENDS to DEPENDS")

Signed-off-by: Luca Boccassi <luca.boccassi@microsoft.com>
Signed-off-by: Khem Raj <raj.khem@gmail.com>

meta-oe/recipes-devtools/python/python-pygobject_3.32.2.bb
@@ -7,7 +7,7 @@ LIC_FILES_CHKSUM = "file://COPYING;md5=a916467b91076e631dd8edb7424769c7"
 GNOMEBASEBUILDCLASS = "meson"
 inherit gnomebase gobject-introspection distutils-base upstream-version-is-even
 
-DEPENDS += "python python-setuptools glib-2.0"
+DEPENDS += "python glib-2.0"
 
 SRCNAME = "pygobject"
 SRC_URI = " \
@@ -30,6 +30,7 @@ PACKAGECONFIG[python] = "-Dpython=python3, -Dpython=python2, , "
 PACKAGECONFIG[stagedir] = "-Dstagedir=${PYTHON_SITEPACKAGES_DIR}, -Dstagedir="", , "
 
 BBCLASSEXTEND = "native"
+RDEPENDS_${PN} = "python-pkgutil"
 RDEPENDS_${PN}_class-native = ""
 
 do_install_append() {