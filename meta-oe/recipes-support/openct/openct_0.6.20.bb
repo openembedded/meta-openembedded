openembedded/meta-openembedded

commit 387824f1763c573fd05c97c4b94122350717340c

openct: Fix rootfs creation errors
To fix error as following:
    file /var/run from install of openct-0.6.20-r0
    conflicts with file from package base-files-3.0.14

Signed-off-by: Dai Caiyun <daicy.fnst@cn.fujitsu.com>
Signed-off-by: Martin Jansa <Martin.Jansa@gmail.com>

meta-oe/recipes-support/openct/openct_0.6.20.bb
@@ -55,6 +55,9 @@ FILES_${PN}-dbg += " \
 
 INSANE_SKIP_${PN} += "dev-deps"
 
+do_install_append() {
+    rm -r ${D}/${localstatedir}/run
+}
 
 do_install () {
     rm -rf ${D}

commit 1ca6398dc591c54a1688ca689bb74bcc256d6141

openct: remove lines that resulted in a no-op
Also remove /run/openct/status from FILES_${PN}, which isn't being
created at all by the recipe – even openct's configure script references
it as /var/run/openct/.

Fixes: 387824f1763c573fd05c (2016-02-28, "openct: Fix rootfs creation errors")
Fixes: 8e61ff98e9fd83aad1a5 (2019-11-21, "openct: merge do_install and do_install_append")
Signed-off-by: Roland Hieber <rhi@pengutronix.de>

meta-oe/recipes-support/openct/openct_0.6.20.bb
@@ -46,7 +46,6 @@ FILES_${PN} += " \
     ${nonarch_base_libdir}/udev \
     ${libdir}/openct-ifd.so \
     ${libdir}/pcsc \
-    /run/openct/status \
 "
 
 FILES_${PN}-dbg += " \
@@ -79,10 +78,4 @@ do_install () {
     so=$(find ${D} -name \*.so | sed "s|^${D}||")
     sed -i -e 's|\\(LIBPATH\\s*\\).*|\\1$so|' etc/reader.conf
     install -Dpm 644 etc/reader.conf ${D}/etc/reader.conf.d/openct.conf
-
-    install -dm 755 ${D}${localstatedir}/run/openct
-    touch ${D}${localstatedir}/run/openct/status
-    chmod 644 ${D}${localstatedir}/run/openct/status
-
-    rm -r ${D}/${localstatedir}/run
 }