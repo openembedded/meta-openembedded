openembedded/meta-openembedded

commit bca3bbbf203086794e5b56a71c04a7bbc59f9bea

curlpp: fix QA Issue after LDFLAGS change
Adding -f*-prefix-map to LDFLAGS caused the following issue:

QA Issue: curlpp.pc failed sanity test (tmpdir)

Fix by filtering out -f*-prefix-map from *.pc files.

[YOCTO #14481]

Signed-off-by: Tony Battersby <tonyb@cybernetics.com>
Signed-off-by: Khem Raj <raj.khem@gmail.com>
(cherry picked from commit c40e01b0fce73bc289d9499b204350359afc7884)
Signed-off-by: Armin Kuster <akuster808@gmail.com>

meta-networking/recipes-support/curlpp/curlpp_0.8.1.bb
@@ -16,3 +16,10 @@ S = "${WORKDIR}/git"
 inherit cmake pkgconfig binconfig
 
 BBCLASSEXTEND = "native nativesdk"
+
+do_install:append() {
+    sed -e 's@[^ ]*-ffile-prefix-map=[^ "]*@@g' \
+        -e 's@[^ ]*-fdebug-prefix-map=[^ "]*@@g' \
+        -e 's@[^ ]*-fmacro-prefix-map=[^ "]*@@g' \
+        -i ${D}${libdir}/pkgconfig/*.pc
+}

commit a82ea8ba0793df5fd4a15e4967eaedd1b3bddf32

curlpp: fix override syntax
Commit bca3bbbf203086794e5b cherry-picked a fix from master, which used
the new override syntax, which was introduced in poky commit
2abf8a699edd513405be (2021-07-25, "bitbake: bitbake: Switch to using new
override syntax"). However, this change was merged after 3.4_M2 and is
not part of hardknott, so bitbake complains about the new syntax:

    ERROR: ParseError at
    …/meta-openembedded/meta-networking/recipes-support/curlpp/curlpp_0.8.1.bb:20:
    unparsed line: 'do_install:append() {'

Revert to the old syntax on the hardknott branch for now.

Fixes: bca3bbbf203086794e5b (2021-08-09, "curlpp: fix QA Issue after LDFLAGS change")
Signed-off-by: Roland Hieber <rhi@pengutronix.de>

meta-networking/recipes-support/curlpp/curlpp_0.8.1.bb
@@ -17,7 +17,7 @@ inherit cmake pkgconfig binconfig
 
 BBCLASSEXTEND = "native nativesdk"
 
-do_install:append() {
+do_install_append() {
     sed -e 's@[^ ]*-ffile-prefix-map=[^ "]*@@g' \
         -e 's@[^ ]*-fdebug-prefix-map=[^ "]*@@g' \
         -e 's@[^ ]*-fmacro-prefix-map=[^ "]*@@g' \