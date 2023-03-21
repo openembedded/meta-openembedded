b021e896a5c03852ad006ccc3df907dc277465b9
Etienne Cordonnier <ecordonnier@snap.com>
2023-03-21 11:03:11 +0100

    android-tools: update to 29.0.6.r14
    Note: the adbd makefile can't be upstreamed to debian, because there is
    already an open PR upstream:
    https://salsa.debian.org/android-tools-team/android-platform-tools/-/merge_requests/4/diffs
    
    Signed-off-by: Etienne Cordonnier <ecordonnier@snap.com>

--- meta-oe/dynamic-layers/selinux/recipes-devtool/android-tools/android-tools/adbd.mk
+++ meta-oe/dynamic-layers/selinux/recipes-devtool/android-tools/android-tools/adbd.mk
@@ -1,18 +1,23 @@
-include ../../rules_yocto.mk
+include rules_yocto.mk
 NAME = adbd
 
 SOURCES = \
-	adb/daemon/main.cpp \
+	adb/adbconnection/adbconnection_server.cpp \
 	adb/daemon/auth.cpp \
-	adb/daemon/jdwp_service.cpp \
 	adb/daemon/file_sync_service.cpp \
+	adb/daemon/file_sync_service.h \
+	adb/daemon/framebuffer_service.cpp \
+	adb/daemon/framebuffer_service.h \
+	adb/daemon/jdwp_service.cpp \
+	adb/daemon/main.cpp \
+	adb/daemon/restart_service.cpp \
+	adb/daemon/restart_service.h \
 	adb/daemon/services.cpp \
 	adb/daemon/shell_service.cpp \
-	adb/daemon/remount_service.cpp \
-	adb/daemon/restart_service.cpp \
-	adb/daemon/reboot_service.cpp \
-	adb/daemon/framebuffer_service.cpp \
-	adb/daemon/set_verity_enable_state_service.cpp \
+	adb/daemon/shell_service.h \
+	adb/daemon/usb_ffs.cpp \
+	adb/daemon/usb_legacy.cpp \
+	adb/daemon/usb.cpp \
 	adb/shell_service_protocol.cpp \
 	adb/adb.cpp \
 	adb/adb_io.cpp \
@@ -20,41 +25,51 @@ SOURCES = \
 	adb/adb_trace.cpp \
 	adb/adb_unique_fd.cpp \
 	adb/adb_utils.cpp \
-	adb/fdevent.cpp \
+	adb/fdevent/fdevent.cpp \
+	adb/fdevent/fdevent_epoll.cpp \
 	adb/services.cpp \
 	adb/sockets.cpp \
 	adb/socket_spec.cpp \
 	adb/sysdeps/errno.cpp \
+	adb/sysdeps/posix/network.cpp \
+	adb/sysdeps_unix.cpp \
 	adb/transport.cpp \
 	adb/transport_fd.cpp \
 	adb/transport_local.cpp \
 	adb/transport_usb.cpp \
-	adb/sysdeps_unix.cpp \
-	adb/sysdeps/posix/network.cpp \
-	adb/daemon/usb_legacy.cpp \
-	adb/daemon/usb_ffs.cpp \
-	adb/daemon/usb.cpp \
+	adb/types.cpp \
 	diagnose_usb/diagnose_usb.cpp \
 	libasyncio/AsyncIO.cpp \
 
+SOURCES := $(foreach source, $(SOURCES), system/core/$(source))
+
+SOURCES += \
+    frameworks/native/libs/adbd_auth/adbd_auth.cpp
+
 CXXFLAGS += -std=gnu++20
-CPPFLAGS += -Iinclude -Iadb -Ibase/include  -I$(OUT_DIR)/usr/include/ -Imkbootimg/include/bootimg -Ifs_mgr/include \
-	    -Ifs_mgr/include_fstab \
+CPPFLAGS += -Isystem/coreinclude -Isystem/core/adb -Isystem/core/base/include  -Idebian/out/system/core -Isystem/tools/mkbootimg/include/bootimg -Isystem/core/fs_mgr/include \
+	    -Isystem/core/fs_mgr/include_fstab \
             -DADB_VERSION='"$(DEB_VERSION)"' -D_GNU_SOURCE
-LDFLAGS += -Wl,-rpath=/usr/lib/$(DEB_HOST_MULTIARCH)/android -Wl,-rpath-link=$(OUT_DIR)/usr/lib/$(DEB_HOST_MULTIARCH)/android/ \
-           -lpthread -L$(OUT_DIR)/usr/lib/$(DEB_HOST_MULTIARCH)/android/ -lbase -lcrypto_utils -lcrypto -lcutils -llog -lresolv
+LDFLAGS += -Wl,-rpath='$$ORIGIN/../lib/android' -Wl,-rpath-link='$$ORIGIN/../lib/android' \
+           -lpthread -Ldebian/out/system/core -Ldebian/out/external/boringssl -lbase -lcrypto_utils -l:libcrypto.a -lcutils -llog -lresolv
 
 PAGE_SIZE ?= 4096
 
 CXXFLAGS += -UADB_HOST
-CXXFLAGS += -DADB_HOST=0
+CXXFLAGS +=	-DADB_HOST=0
 CXXFLAGS += -DALLOW_ADBD_DISABLE_VERITY
 CXXFLAGS += -DALLOW_ADBD_NO_AUTH
-CXXFLAGS += -DPLATFORM_TOOLS_VERSION='"28.0.2"'
-CXXFLAGS += -Idiagnose_usb/include
-CXXFLAGS += -Iadb/daemon/include
-CXXFLAGS += -Ilibasyncio/include
-CXXFLAGS += -Wno-c++11-narrowing
+CXXFLAGS += -DPLATFORM_TOOLS_VERSION='"28.0.2"' 
+CXXFLAGS += -Isystem/core/diagnose_usb/include 
+CXXFLAGS += -Isystem/core/adb/daemon/include
+CXXFLAGS += -Isystem/core/adb/adbconnection/include
+CXXFLAGS += -Isystem/core/libasyncio/include
+CXXFLAGS += -Isystem/core/libcutils/include
+CXXFLAGS += -Isystem/core/libcrypto_utils/include
+CXXFLAGS += -Isystem/core/liblog/include/
+CXXFLAGS += -Isystem/core/libutils/include
+CXXFLAGS += -Iframeworks/native/libs/adbd_auth/include
+CXXFLAGS += -Wno-c++11-narrowing 
 CXXFLAGS += -DPAGE_SIZE=$(PAGE_SIZE)
 
 
@@ -65,8 +80,8 @@ ifneq ($(filter armel mipsel,$(DEB_HOST_ARCH)),)
 endif
 
 build: $(SOURCES)
-	mkdir --parents ../../../android-tools/adbd
-	$(CXX) $^ -o ../../../android-tools/adbd/adbd $(CXXFLAGS) $(CPPFLAGS) $(LDFLAGS)
+	mkdir --parents debian/out/system/core
+	$(CXX) $^ -o debian/out/system/core/adbd $(CXXFLAGS) $(CPPFLAGS) $(LDFLAGS)
 
 clean:
-	$(RM) $(OUT_DIR)/usr/bin/$(NAME)
+	$(RM) debian/out/system/core/adbd
