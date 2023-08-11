openembedded/meta-openembedded

commit 6be235b5c3b7c7a12c209c897df03d613ebda3c1

libiio: update to version 0.25
The version 0.25 has been released, it has not been tagged however.
Update the this version anyway.

Signed-off-by: Marek Vasut <marex@denx.de>
Signed-off-by: Khem Raj <raj.khem@gmail.com>

meta-oe/recipes-support/libiio/libiio_git.bb
@@ -4,8 +4,8 @@ SECTION = "libs"
 LICENSE = "LGPL-2.1-or-later"
 LIC_FILES_CHKSUM = "file://COPYING.txt;md5=7c13b3376cea0ce68d2d2da0a1b3a72c"
 
-SRCREV = "c4498c27761d04d4ac631ec59c1613bfed079da5"
-PV = "0.24"
+SRCREV = "eee8cba6f42177a094ebe16759606a719676fda7"
+PV = "0.25"
 
 SRC_URI = "git://github.com/analogdevicesinc/libiio.git;protocol=https;branch=master"
 UPSTREAM_CHECK_GITTAGREGEX = "v(?P<pver>\d+(\.\d+)+)"

commit 98b7f5c8ea1990d3109f7cbcf414785babdc4e31

libiio: Use tagged v0.25
Use the actual tagged libiio v0.25 , not some random older version.

Fixes: 6be235b5c ("libiio: update to version 0.25")
Signed-off-by: Marek Vasut <marex@denx.de>
Signed-off-by: Khem Raj <raj.khem@gmail.com>

meta-oe/recipes-support/libiio/libiio_git.bb
@@ -4,7 +4,7 @@ SECTION = "libs"
 LICENSE = "LGPL-2.1-or-later"
 LIC_FILES_CHKSUM = "file://COPYING.txt;md5=7c13b3376cea0ce68d2d2da0a1b3a72c"
 
-SRCREV = "eee8cba6f42177a094ebe16759606a719676fda7"
+SRCREV = "b6028fdeef888ab45f7c1dd6e4ed9480ae4b55e3"
 PV = "0.25"
 
 SRC_URI = "git://github.com/analogdevicesinc/libiio.git;protocol=https;branch=master"