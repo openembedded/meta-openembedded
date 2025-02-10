openembedded/meta-openembedded

commit 4271cc28279b79140fc6bbde32c9110aec8d1d86

android-tools: Add flag to enable adbd service (#147)
android-tools-adbd service can be enabled in the image using
USB_DEBUGGING_ENABLED = "1" in local.conf.

Signed-off-by: Devendra Tewari <devendra.tewari@gmail.com>
Signed-off-by: Khem Raj <raj.khem@gmail.com>

meta-oe/recipes-devtools/android-tools/android-tools_5.1.1.r37.bb
@@ -180,3 +180,9 @@ FILES:${PN}-fstools = "\
 "
 
 BBCLASSEXTEND = "native"
+
+android_tools_enable_devmode() {
+    touch ${IMAGE_ROOTFS}/var/usb-debugging-enabled
+}
+
+ROOTFS_POSTPROCESS_COMMAND_${PN}-adbd += "${@bb.utils.contains("USB_DEBUGGING_ENABLED", "1", "android_tools_enable_devmode;", "", d)}"

commit 6b44831860baf6285afa75f80f5108f44a7e431a

android-tools: drop useless USB_DEBUGGING_ENABLED handling
This reverts commits 4271cc28279b ("android-tools: Add flag to enable
adbd service (#147)") and b85ae59968f4 ("android-tools 10: Add flag to
enable adbd service"). These two commits added optional
ROOTFS_POSTPROCESS_COMMAND touching the flag file, however this variable
doesn't make any sense in the package recipe, it is only concerned in
the image context.

Drop useless code from the android-tools recipe.

Fixes: 4271cc28279b ("android-tools: Add flag to enable adbd service (#147)")
Fixes: b85ae59968f4 ("android-tools 10: Add flag to enable adbd service")
Signed-off-by: Dmitry Baryshkov <dmitry.baryshkov@linaro.org>
Signed-off-by: Khem Raj <raj.khem@gmail.com>

meta-oe/dynamic-layers/selinux/recipes-devtool/android-tools/android-tools_29.0.6.r14.bb
@@ -188,9 +188,3 @@ FILES:${PN}-fstools = "\
 FILES:${PN} += "${libdir}/android ${libdir}/android/*"
 
 BBCLASSEXTEND = "native"
-
-android_tools_enable_devmode() {
-    touch ${IMAGE_ROOTFS}/etc/usb-debugging-enabled
-}
-
-ROOTFS_POSTPROCESS_COMMAND_${PN}-adbd += "${@bb.utils.contains("USB_DEBUGGING_ENABLED", "1", "android_tools_enable_devmode;", "", d)}"

meta-oe/recipes-devtools/android-tools/android-tools_5.1.1.r37.bb
@@ -188,12 +188,6 @@ FILES:${PN}-fstools = "\
 
 BBCLASSEXTEND = "native"
 
-android_tools_enable_devmode() {
-    touch ${IMAGE_ROOTFS}/etc/usb-debugging-enabled
-}
-
-ROOTFS_POSTPROCESS_COMMAND_${PN}-adbd += "${@bb.utils.contains("USB_DEBUGGING_ENABLED", "1", "android_tools_enable_devmode;", "", d)}"
-
 # http://errors.yoctoproject.org/Errors/Details/766881/
 # android-tools/5.1.1.r37/git/system/core/adb/adb_auth_host.c:86:23: error: passing argument 2 of 'RSA_get0_key' from incompatible pointer type [-Wincompatible-pointer-types]
 # android-tools/5.1.1.r37/git/system/core/adb/adb_auth_host.c:86:27: error: passing argument 3 of 'RSA_get0_key' from incompatible pointer type [-Wincompatible-pointer-types]