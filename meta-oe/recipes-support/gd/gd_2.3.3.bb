openembedded/meta-openembedded

commit be9f029b6c104b0813b36b3dcfa623ad5626d76f

gd: Support PACKAGECONFIG
Some recipes (like fswebcam) need gd with support of fontconfig to really
work. Otherwise font selection is not that easy.

Signed-off-by: Jörg Sommer <joerg.sommer@navimatix.de>
Signed-off-by: Khem Raj <khem.raj@oss.qualcomm.com>

meta-oe/recipes-support/gd/gd_2.3.3.bb
@@ -23,14 +23,23 @@ SRCREV = "b5319a41286107b53daa0e08e402aa1819764bdc"
 
 inherit autotools binconfig gettext pkgconfig
 
-EXTRA_OECONF += " --disable-rpath \
-                  --with-jpeg=${STAGING_LIBDIR}/.. \
-                  --with-freetype=yes \
-                  --without-fontconfig \
-                  --without-webp \
-                  --without-xpm \
-                  --without-x \
-                "
+PACKAGECONFIG ?= "jpeg freetype"
+
+PACKAGECONFIG[avif] = "--with-avif,--without-avif"
+PACKAGECONFIG[fontconfig] = "--with-fontconfig,--without-fontconfig"
+PACKAGECONFIG[freetype] = "--with-freetype,--without-freetype"
+PACKAGECONFIG[heif] = "--with-heif,--without-heif"
+PACKAGECONFIG[jpeg] = "--with-jpeg,--without-jpeg"
+PACKAGECONFIG[liq] = "--with-liq,--without-liq"
+PACKAGECONFIG[png] = "--with-png,--without-png"
+PACKAGECONFIG[raqm] = "--with-raqm,--without-raqm,libraqm"
+PACKAGECONFIG[tiff] = "--with-tiff,--without-tiff"
+PACKAGECONFIG[webp] = "--with-webp,--without-webp"
+PACKAGECONFIG[x] = "--with-x,--without-x"
+PACKAGECONFIG[xpm] = "--with-xpm,--without-xpm"
+PACKAGECONFIG[zlib] = "--with-zlib,--without-zlib"
+
+EXTRA_OECONF += "--disable-rpath"
 
 EXTRA_OEMAKE = 'LDFLAGS="${LDFLAGS}"'
 

commit e5ba52fccab35e6a4564b7039778f24f12c0771f

gd: add missing library deps to PACKAGECONFIG entries
The PACKAGECONFIG entries introduced by the conversion were all missing
their build-dependency in the third field (the only exception was raqm,
which already had libraqm there).  As a result the hard-coded DEPENDS
line was compensating by always pulling in every library regardless of
which features were actually selected.

Evidence from the configure logs confirms which libraries were found or
missed across the three configurations:

  pre-conversion:
    checking for zlib... yes
    checking for libpng... yes
    checking for freetype2 >= 9.8.3... yes
    checking for libjpeg... yes
    checking for libtiff-4... yes

  post-conversion, unfixed (zlib, png, and tiff checks absent entirely):
    checking for freetype2 >= 9.8.3... yes
    checking for libjpeg... yes

  post-conversion, fixed:
    checking for zlib... yes
    checking for libpng... yes
    checking for freetype2 >= 9.8.3... yes
    checking for libjpeg... yes
    checking for libtiff-4... yes

Add the correct Yocto package name to the third field of every entry:
  avif       -> libavif
  fontconfig -> fontconfig
  freetype   -> freetype
  heif       -> libheif
  jpeg       -> jpeg
  liq        -> libimagequant
  png        -> libpng
  tiff       -> tiff
  webp       -> libwebp
  x          -> virtual/libx11
  xpm        -> libxpm
  zlib       -> zlib

With the dependencies now managed by PACKAGECONFIG, the unconditional
DEPENDS assignment is redundant and can be removed.

Fixes: be9f029b6c10 ("gd: Support PACKAGECONFIG")
AI-Generated: codex/claude-sonnet 4.6 (high)
Signed-off-by: Trevor Woerner <twoerner@gmail.com>
Signed-off-by: Khem Raj <khem.raj@oss.qualcomm.com>

meta-oe/recipes-support/gd/gd_2.3.3.bb
@@ -11,8 +11,6 @@ SECTION = "libs"
 LICENSE = "GD"
 LIC_FILES_CHKSUM = "file://COPYING;md5=ace63adfdac78400fc30fa22ee9c1bb1"
 
-DEPENDS = "freetype libpng jpeg zlib tiff"
-
 SRC_URI = "git://github.com/libgd/libgd.git;nobranch=1;protocol=https \
            file://0001-Fix-deprecared-function-prototypes.patch \
            file://Fix-ftype-missing-const.patch \
@@ -25,19 +23,19 @@ inherit autotools binconfig gettext pkgconfig
 
 PACKAGECONFIG ?= "jpeg png zlib tiff freetype"
 
-PACKAGECONFIG[avif] = "--with-avif,--without-avif"
-PACKAGECONFIG[fontconfig] = "--with-fontconfig,--without-fontconfig"
-PACKAGECONFIG[freetype] = "--with-freetype,--without-freetype"
-PACKAGECONFIG[heif] = "--with-heif,--without-heif"
-PACKAGECONFIG[jpeg] = "--with-jpeg,--without-jpeg"
-PACKAGECONFIG[liq] = "--with-liq,--without-liq"
-PACKAGECONFIG[png] = "--with-png,--without-png"
+PACKAGECONFIG[avif] = "--with-avif,--without-avif,libavif"
+PACKAGECONFIG[fontconfig] = "--with-fontconfig,--without-fontconfig,fontconfig"
+PACKAGECONFIG[freetype] = "--with-freetype,--without-freetype,freetype"
+PACKAGECONFIG[heif] = "--with-heif,--without-heif,libheif"
+PACKAGECONFIG[jpeg] = "--with-jpeg,--without-jpeg,jpeg"
+PACKAGECONFIG[liq] = "--with-liq,--without-liq,libimagequant"
+PACKAGECONFIG[png] = "--with-png,--without-png,libpng"
 PACKAGECONFIG[raqm] = "--with-raqm,--without-raqm,libraqm"
-PACKAGECONFIG[tiff] = "--with-tiff,--without-tiff"
-PACKAGECONFIG[webp] = "--with-webp,--without-webp"
-PACKAGECONFIG[x] = "--with-x,--without-x"
-PACKAGECONFIG[xpm] = "--with-xpm,--without-xpm"
-PACKAGECONFIG[zlib] = "--with-zlib,--without-zlib"
+PACKAGECONFIG[tiff] = "--with-tiff,--without-tiff,tiff"
+PACKAGECONFIG[webp] = "--with-webp,--without-webp,libwebp"
+PACKAGECONFIG[x] = "--with-x,--without-x,virtual/libx11"
+PACKAGECONFIG[xpm] = "--with-xpm,--without-xpm,libxpm"
+PACKAGECONFIG[zlib] = "--with-zlib,--without-zlib,zlib"
 
 EXTRA_OECONF += "--disable-rpath"
 