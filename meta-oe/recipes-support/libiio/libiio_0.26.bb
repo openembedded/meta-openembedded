openembedded/meta-openembedded

commit 9b5fc1605be1dfd44bbbe6a1f06fd4d5655ebb7f

libiio: Add sysvinit support
Libiio can also be built with a sysvinit init script. Hence, enable it
as iiod still runs on some system running sysvinit init scripts.

Signed-off-by: Nuno Sá <nuno.sa@analog.com>
Signed-off-by: Khem Raj <raj.khem@gmail.com>

meta-oe/recipes-support/libiio/libiio_0.26.bb
@@ -16,7 +16,7 @@ DEPENDS = " \
     flex-native bison-native libaio \
 "
 
-inherit cmake python3native systemd setuptools3 pkgconfig
+inherit cmake python3native systemd setuptools3 pkgconfig update-rc.d
 
 EXTRA_OECMAKE = " \
     -DCMAKE_BUILD_TYPE=RelWithDebInfo \
@@ -25,6 +25,7 @@ EXTRA_OECMAKE = " \
     -DBISON_TARGET_ARG_COMPILE_FLAGS='--no-lines' \
     -DUDEV_RULES_INSTALL_DIR=${nonarch_base_libdir}/udev/rules.d \
     ${@bb.utils.contains('DISTRO_FEATURES', 'systemd', '-DWITH_SYSTEMD=ON -DSYSTEMD_UNIT_INSTALL_DIR=${systemd_system_unitdir}', '', d)} \
+    ${@bb.utils.contains('DISTRO_FEATURES', 'sysvinit', '-DWITH_SYSVINIT=on', '', d)} \
 "
 
 PACKAGECONFIG ??= " \
@@ -59,6 +60,7 @@ FILES:${PN}-python3 = "${PYTHON_SITEPACKAGES_DIR}"
 
 SYSTEMD_PACKAGES = "${PN}-iiod"
 SYSTEMD_SERVICE:${PN}-iiod = "iiod.service"
+INITSCRIPT_NAME = "${@bb.utils.contains('DISTRO_FEATURES', 'sysvinit', 'iiod', '', d)}"
 
 # Explicitly define do_configure, do_compile and do_install because both cmake and setuptools3 have
 # EXPORT_FUNCTIONS do_configure do_compile do_install

commit d67ee354f23de378044f1a5211fe44c7725f1fe0

libiio: Move sysvinit script to libiio-iiod package
There is no use in the init script outside of iiod package. Package it
accordingly.

Fixes: 9b5fc1605be1 ("libiio: Add sysvinit support")
Signed-off-by: Alexander Stein <alexander.stein@ew.tq-group.com>
Signed-off-by: Khem Raj <khem.raj@oss.qualcomm.com>

meta-oe/recipes-support/libiio/libiio_0.26.bb
@@ -52,6 +52,7 @@ RDEPENDS:${PN}:remove = "python3-core"
 RDEPENDS:${PN}-python3 = "${PN} python3-core python3-ctypes python3-stringold"
 
 FILES:${PN}-iiod = " \
+    ${sysconfdir}/init.d/iiod \
     ${sbindir}/iiod \
     ${systemd_system_unitdir}/iiod.service \
 "
@@ -60,7 +61,8 @@ FILES:${PN}-python3 = "${PYTHON_SITEPACKAGES_DIR}"
 
 SYSTEMD_PACKAGES = "${PN}-iiod"
 SYSTEMD_SERVICE:${PN}-iiod = "iiod.service"
-INITSCRIPT_NAME = "${@bb.utils.contains('DISTRO_FEATURES', 'sysvinit', 'iiod', '', d)}"
+INITSCRIPT_PACKAGES = "${PN}-iiod"
+INITSCRIPT_NAME:${PN}-iiod = "${@bb.utils.contains('DISTRO_FEATURES', 'sysvinit', 'iiod', '', d)}"
 
 # Explicitly define do_configure, do_compile and do_install because both cmake and setuptools3 have
 # EXPORT_FUNCTIONS do_configure do_compile do_install